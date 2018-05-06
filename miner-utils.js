8af8dd19b83c699c954e4242498047c4062f53bd
theminor <theminor@gmail.com>
2018-05-06 13:06:11 -0400

    starting basic function

--- miner-utils.js
+++ miner-utils.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const { promisify } = require('util');
+const net = require('net');
+const connectAsync = promisify(net.connect);
+
+const cgCmd = async function(command, config) {
+	var socket;
+	var dataStg = '';
+	try {
+		socket = await connectAsync({ host: config.ip, port: config.port });
+	} catch (err) {
+		return node.error('Error in net socket connection: ' + err);
+	}
+	socket.on('data', res => (dataStg += res.toString()));
+	socket.on('end', () => {
+		socket.removeAllListeners();
+		try {
+			return JSON.parse(
+				dataStg.replace(/\u0000/g, '').replace(/}{/g, '},{')
+			);
+		} catch (err) {
+			node.warn('Error parsing json: ' + err);
+			return dataStg;
+		}
+	});
+	socket.on('error', err => {
+		socket.removeAllListeners();
+		return node.error('Net socket error: ' + err);
+	});
+};
