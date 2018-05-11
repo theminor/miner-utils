9bccc2e788678c97f0d4092c429c8e36bfdf949c
John <theminor@users.noreply.github.com>
2018-05-11 08:42:48 -0400

    tidy netCmd()

--- miner-utils.js
+++ miner-utils.js
@@ -1,31 +1,23 @@
 'use strict';
+const { connect } = require('net');
 
-const { promisify } = require('util');
-const net = require('net');
-const connectAsync = promisify(net.connect);
-
-const cgCmd = async function(command, config) {
-	var socket;
-	var dataStg = '';
-	try {
-		socket = await connectAsync({ host: config.ip, port: config.port });
-	} catch (err) {
-		return node.error('Error in net socket connection: ' + err);
-	}
-	socket.on('data', res => (dataStg += res.toString()));
-	socket.on('end', () => {
-		socket.removeAllListeners();
-		try {
-			return JSON.parse(
-				dataStg.replace(/\u0000/g, '').replace(/}{/g, '},{')
-			);
-		} catch (err) {
-			node.warn('Error parsing json: ' + err);
-			return dataStg;
-		}
-	});
-	socket.on('error', err => {
-		socket.removeAllListeners();
-		return node.error('Net socket error: ' + err);
+const netCmd = function(command, config) {
+	return new Promise( (resolve, reject) => {
+		let socket, timeoutTimer, dataStg = '';
+		const disconnectReject = function(reason) {
+			if (socket.removeAllListeners) socket.removeAllListeners();
+			reject(reason);
+		};
+		if (config.timeOut) timeoutTimer = setTimeout( () => disconnectReject('Socket timeout'), config.timeOut );
+		try { socket = connect({ host: config.ip || '127.0.0.1', port: config.port || 4028 }); }
+		catch (err) { disconnectReject('Socket connect error: ' + err); }
+		socket.on('data', res => dataStg += res.toString());
+		socket.on('error', err => disconnectReject('Socket net error: ' + err));
+		socket.on('end', () => {
+			socket.removeAllListeners();
+			try { resolve( JSON.parse(dataStg.replace(/\u0000/g, '').replace(/}{/g, '},{')) ); }
+			catch (err) { resolve(dataStg); }			
+		});
+		socket.write(command);
 	});
 };
